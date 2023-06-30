38941adbaf5e02974fae8c65e5791f62ef48b15a
eevgeniy <evlevgenij@yandex.ru>
2023-06-30 20:02:12 +0400

    начало решение задача 2

--- Program.cs
+++ Program.cs
@@ -4,20 +4,45 @@
 
 
 
-Console.WriteLine("Введите число:");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число:");
-int B = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число:");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число:");
+// int B = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"{A} в степени {B} -> {Pow(A,B)}");
+// Console.WriteLine($"{A} в степени {B} -> {Pow(A,B)}");
 
-int Pow(int num, int num2)
+// int Pow(int num, int num2)
+// {
+//     if(B==0) return 1;
+//     int result = num;
+//     for(int i = 2; i <= num2; i++)
+//     {
+//         result *= num;
+//     }
+//     return result;
+// }
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{number} -> {GetSum(number)}");
+
+int GetSum(int num)
 {
-    if(B==0) return 1;
-    int result = num;
-    for(int i = 2; i <= num2; i++)
+    int sum = 0;
+    while (num>0)
     {
-        result *= num;
+        sum+=num%10;
+        num/=10;
     }
-    return result;
+    return sum;
 }
